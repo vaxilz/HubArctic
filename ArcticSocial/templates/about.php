775236af9f6aac3822849b50f88305a42c5e2360
IntenseBacon <fallen_m_2009@hotmail.com>
2016-12-20 14:18:33 -0400

    about.php
    about.php

---
+++ ArcticSocial/templates/about.php
@@ -0,0 +1,97 @@
+<!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">About
+                    <small>ArcticSocial</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Home</a>
+                    </li>
+                    <li class="active">About</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-md-3">
+                <img src="images/Icon.png" alt=""/>
+            </div>
+            <div class="col-md-6">
+              <h2>About ArcticSocial </h2>
+              <p>ArcticSocial was created by 3 College friends looking to bring people together in many different ways.</p>
+                <p>Throught the idea of bringing people we develeoped a hosting service to brings that dream to reality no matter what the party is.</p>
+                <p></p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+            <div class="col-lg-12">
+              <h2 class="page-header">Our Team </h2>
+              
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img src="images/breadly.jpg" width="250" height="50" alt=""/>
+                    <div class="caption">
+                        <h3>Bradley Perrin<br>
+                            <small>Developer | Host</small>
+                        </h3>
+                        <p>This is brads bio to be.</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                   <img  src="images/markMcphail.jpg" alt=""
+                   class="img-responsive"/>
+                    <div class="caption">
+                        <h3>Mark McPhail<br>
+                            <small>Developer | Host</small>
+                        </h3>
+                        <p>Graduated from Clement-Cormier in 2016,Enjoys music and playing guitar and video games. Mark is currently attending Oulton College to continue on in his programing
+carreer in hopes to one day open a business of his own.</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img src="images/Luc_Petit_Land.jpg" alt=""
+                   class="img-responsive"/>
+                    <div class="caption">
+                        <h3>Luc Poirier<br>
+                            <small>Developer | Host</small>
+                        </h3>
+                        <p>Gratuated from LJR in 2016, he love sports, video games and idea solving. He is ready to take a challenge. <br>He is a big fan of team work but he is not affraid to work alone.</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li> 
+                        </ul>
+                    </div>
+                </div>
+            </div>
+          
+                        
+      </div>
+                </div>
+        <hr>
